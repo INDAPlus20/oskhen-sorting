763bde07003968e58258712fc36f80d421f6c8dc
oskhen <oskar.henriksson2001@gmail.com>
2020-11-25 19:24:11 +0100

    Replaced counting sort with radix sort

--- pyimpl/sorting.py
+++ pyimpl/sorting.py
@@ -1,3 +1,5 @@
+from math import log10, floor
+
 ## Selection Sort
 def select_sort(x):
     for i in range(len(x)):
@@ -57,18 +59,6 @@ def merge_sort(x):
 
     return merge(left, right)
 
-## Counting Sort
-
-def count_sort(x):
-    maxi = max(x)
-    count = [0]*(maxi+1) #Arrays start at 0
-    for val in x:
-        count[val] += 1
-    output = list()
-    for index, element in enumerate(count):
-        output += [index]*element
-    return output
-
 ## Heap Sort
 
 def heap_sort(x):
@@ -99,8 +89,46 @@ def heap_sort(x):
         heapify(heap, 0, i+1)
         heap[i], heap[0] = heap[0], heap[i]
     return(heap)
+
+## Radix sort
+
+def radix_sort(x):
+
+    def counting_sort(values, keys): # Assumes keys are integers and keys[i] is the key for the value values[i]
+        output = [0] * len(values)
+        freq = [0] * (max(keys) + 1)
+
+        for element in keys:
+            freq[element] += 1
+        
+        for i in range(1,len(freq)):
+            freq[i] += freq[i-1]
+        
+
+        for i in range(len(values)-1, -1, -1):
+            output[freq[keys[i]]-1] = values[i] #-1 bcs arrays start at 0
+            freq[keys[i]] -= 1
+
+        return output
+
+    maxi = max(x)
+
+    if maxi > 0:
+        maxi = floor(log10(maxi) + 1)
+    else:
+        maxi = 1
+
+    for radix in range(0, maxi):
+        keys = [(element // 10**radix % 10) for element in x]
+        x = counting_sort(x, keys)
     
+    return x
 
 
 if __name__ == "__main__":
-    pass
+    a = [7, 2, 9, 8, 9, 4, 4, 4, 1, 11, 1, 0, 10, 3, 5, 6]
+    b = [i+.03 for i in a]
+    c = [4, 2, 3, 1]
+    d = [1, 3, 1]
+    e = [7, 2, 9, 8, 9, 4, 4, 4, 1, 1, 0, 3, 5, 6]
+    print(radix_sort(a))
